03cb365e7b10a35e6340e41456d273cb5efe0315
romklao <rk120879@gmail.com>
2017-06-26 01:15:29 -0700

    Updated styling

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 'use strict'
 
+function moveToAbout() {
+    $('.linkToAbout').click(function(event) {
+        event.preventDefault();
+    $('html, body').animate({
+        scrollTop: parseInt($(".skillPortfolioWrap").offset().top)
+    }, 1200);
+  });
+}
+
 function moveToPortfolio() {
     $('.linkToPortfolio').click(function(event) {
         event.preventDefault();
     $('html, body').animate({
         scrollTop: parseInt($("#portfolioPageWrap").offset().top)
-    }, 1500);
+    }, 1200);
   });
 }
 
@@ -14,11 +23,40 @@ function moveToContact() {
         event.preventDefault();
     $('html, body').animate({
         scrollTop: parseInt($("#contactWrap").offset().top)
-    }, 1500);
+    }, 1200);
   });
 }
 
+function scrollNavChange(){         
+   var scroll_start = 0;
+   var startchange = $('.skillPortfolioWrap');
+   var offset = startchange.offset();
+
+    if (startchange.length) {
+        $(document).scroll(function() { 
+            scroll_start = $(this).scrollTop();
+
+            if(scroll_start > offset.top) {
+              $(".navbar").css({'background': 'white', 
+                'border-bottom': '1px solid #edeaea'});
+            } else {
+              $('.navbar-default').css({'background': 'url(images/background2.jpg)', 
+                'background-size': 'cover', 'border-bottom': 'none'});
+            }
+        });
+    }
+}
+
 $(function() {
+    moveToAbout();
     moveToPortfolio();
     moveToContact();
+    scrollNavChange();
 }) 
+
+
+
+
+
+
+
