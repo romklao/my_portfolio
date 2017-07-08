d057060414ba1575ac5401bb45376ffb37d76308
romklao <rk120879@gmail.com>
2017-07-07 22:23:53 -0700

    Updated about div

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function moveToAbout() {
     $('.linkToAbout').click(function(event) {
         event.preventDefault();
     $('html, body').animate({
-        scrollTop: parseInt($(".skillPortfolioWrap").offset().top)
+        scrollTop: parseInt($(".aboutTitle").offset().top)
     }, 1000);
   });
 }
@@ -67,20 +67,19 @@ var total = 20;
 var warp = document.getElementById("landingPage"),  w = window.innerWidth , h = window.innerHeight;
  
 for (var i=0; i<total; i++){ 
-   var Div = document.createElement('div');
-   TweenLite.set(Div,{attr:{class:'dot'},x:R(0,w),y:R(-200,-150),z:R(-200,200)});
-   warp.appendChild(Div);
-   animm(Div);
+    var Div = document.createElement('div');
+    TweenLite.set(Div,{attr:{class:'dot'},x:R(0,w),y:R(-200,-150),z:R(-200,200)});
+    warp.appendChild(Div);
+    animm(Div);
 }
  
 function animm(elm){   
-   TweenMax.to(elm,R(6,15),{y:h+100,ease:Linear.easeNone,repeat:-1,delay:-15});
-   TweenMax.to(elm,R(4,8),{x:'+=100',rotationZ:R(0,180),repeat:-1,yoyo:true,ease:Sine.easeInOut});
-   TweenMax.to(elm,R(2,8),{rotationX:R(0,360),rotationY:R(0,360),repeat:-1,yoyo:true,ease:Sine.easeInOut,delay:-5});
- };
+    TweenMax.to(elm,R(6,15),{y:h+100,ease:Linear.easeNone,repeat:-1,delay:-15});
+    TweenMax.to(elm,R(4,8),{x:'+=100',rotationZ:R(0,180),repeat:-1,yoyo:true,ease:Sine.easeInOut});
+    TweenMax.to(elm,R(2,8),{rotationX:R(0,360),rotationY:R(0,360),repeat:-1,yoyo:true,ease:Sine.easeInOut,delay:-5});
+};
 
 function R(min,max) {return min+Math.random()*(max-min)};
-
 
 
 $(function() {
